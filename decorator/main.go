a865f28bbfce402d8b73a5b5cc5e10c7c4747afc
daidevgo <daidevgo@gmail.com>
2023-03-01 23:30:23 +0700

    add decorator

---
+++ decorator/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type IProcess interface {
+	process()
+}
+
+type ProcessClass struct{}
+
+func (process *ProcessClass) process() {
+	fmt.Println("ProcessClass process")
+}
+
+type ProcessDecorator struct {
+	processInstance *ProcessClass
+}
+
+func (decorator *ProcessDecorator) process() {
+	if decorator.processInstance == nil {
+		fmt.Println("ProcessDecorator process")
+	} else {
+		fmt.Print("ProcessDecorator process and ")
+		decorator.processInstance.process()
+	}
+}
+
+func main() {
+	var process = &ProcessClass{}
+	var decorator = &ProcessDecorator{}
+
+	decorator.process()
+	decorator.processInstance = process
+	decorator.process()
+}
